3c3dddac68ea244d2f7c6cdebb339a39e92ee52b
Gandalf- <austin.voecks@gmail.com>
2018-03-25 08:21:17 -0700

    more updates for pypi, version 1.0.0

--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
 from distutils.core import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version = '1.0.0'
+download = \
+    'https://github.com/Gandalf-/apocrypha/archive/' + version + 'tar.gz'
+
 setup(
-  name='apocrypha',
-  packages=['apocrypha'],
-  version='0.1',
-  description='A lightweight, flexible JSON server and client',
-  author='Austin Voecks',
-  author_email='austin.voecks@gmail.com',
-  url='https://github.com/Gandalf-/apocrypha',
-  download_url='https://github.com/Gandalf-/apocrypha/archive/0.1.tar.gz',
-  keywords=['database', 'json'],
-  classifiers=[],
+    name='apocrypha',
+    packages=['apocrypha'],
+    version=version,
+    description='A lightweight, flexible JSON server and client',
+    long_description=long_description,
+    python_requires='>=3',
+    author='Austin Voecks',
+    author_email='austin.voecks@gmail.com',
+    url='https://github.com/Gandalf-/apocrypha',
+    download_url=download,
+    keywords=['database', 'json'],
+    classifiers=[
+        'Development Status :: 2 - Beta',
+        'Programming Language :: Python :: 3',
+    ],
+    project_urls={
+        'Bug Reports': 'https://github.com/Gandalf-/apocrypha/issues',
+        'Source': 'https://github.com/Gandalf-/apocrypha',
+    },
 )

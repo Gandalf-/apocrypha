f55a3341fee0a9c025a273a9de9e8d77d6161326
Gandalf- <austin.voecks@gmail.com>
2018-03-25 08:40:58 -0700

    reorganize for setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md')) as f:
+with open(path.join(here, 'README.txt')) as f:
     long_description = f.read()
 
-version = '1.0.2'
+version = '1.0.0'
 download = \
     'https://github.com/Gandalf-/apocrypha/archive/' + version + 'tar.gz'
 
@@ -17,7 +17,6 @@ setup(
     version=version,
     description='A lightweight, flexible JSON server and client',
     long_description=long_description,
-    python_requires='>=3',
     author='Austin Voecks',
     author_email='austin.voecks@gmail.com',
     url='https://github.com/Gandalf-/apocrypha',
@@ -27,8 +26,4 @@ setup(
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3',
     ],
-    project_urls={
-        'Bug Reports': 'https://github.com/Gandalf-/apocrypha/issues',
-        'Source': 'https://github.com/Gandalf-/apocrypha',
-    },
 )

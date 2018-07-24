3fcf911433be905df573dd0d1d5478e803c7d9e5
Gandalf- <austin.voecks@gmail.com>
2018-07-23 20:53:46 -0700

    update coverage.sh

--- test/coverage.sh
+++ test/coverage.sh
@@ -6,5 +6,6 @@ coverage run --branch -a --source apocrypha/ test/test_database_unit.py
 coverage run --branch -a --source apocrypha/ test/test_server.py
 coverage run --branch -a --source apocrypha/ test/test_node.py
 coverage run --branch -a --source apocrypha/ test/test_network.py
+coverage run --branch -a --source apocrypha/ test/test_datum.py
 
 coverage html
